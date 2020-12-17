bbfaf620c8a695d806ba61fab072393d4d980673
Jordan Newberry <jordan.newberry@gmail.com>
2020-12-17 10:09:56 -0600

    base file structure set up with dependencies installed.

---
+++ src/components/SearchList/SearchList.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class SearchList extends Component{
+    render(){
+        return(
+            <div>
+                <h2>hello from SearchList component</h2>
+            </div>
+        ) // end return
+    } // end render
+} // end class
+
+export default SearchList;
