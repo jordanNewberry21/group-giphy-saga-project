07131166354257c2f935d62df40ed893347c5ab0
James Posey <jamesrposeyjr@gmail.com>
2020-12-17 13:08:27 -0600

    fixed import statements made header

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,13 +1,12 @@
-import { Component } from 'react';
-
-class Favorites extends Component{
-    render(){
-        return(
-            <div>
-                <h2>hello from Favorites component</h2>
-            </div>
-        ) // end return
-    } // end render
+import React, { Component } from 'react';
+class Favorites extends Component {
+  render() {
+    return (
+      <div>
+        <h2>hello from Favorites component</h2>
+      </div>
+    ); // end return
+  } // end render
 } // end class
 
 export default Favorites;
