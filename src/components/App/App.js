221936927044388347265ff2c04211f36d791616
Steven Maloney <steven.maloney@gmail.com>
2020-12-17 12:49:35 -0600

    routes retry

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {HashRouter as Router, Route, Link} from 'react-router-dom'; 
+import '../../components/Search/Search';
+import '../../components/Favorites/Favorites';
 
 
 class App extends Component {
@@ -7,6 +10,17 @@ class App extends Component {
     return (
       <div>
         <h1>Giphy Search!</h1>
+        <Router>
+          <nav>
+            <ul>
+              <li><Link to="/">Search</Link></li>
+              <li><Link to="/favorites">Favorites</Link></li>
+            </ul>
+          </nav>
+
+          <Route exact path="/" component={Search}/>
+          <Route path="/favorites" component={Favorites}/>
+        </Router>
       </div>
     );
   }
