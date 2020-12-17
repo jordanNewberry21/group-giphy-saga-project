195082848881053f1e0f813109cf57bc05a3106a
Jordan Newberry <jordan.newberry@gmail.com>
2020-12-17 10:33:44 -0600

    initial redux-saga setup for index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import axios from 'axios';
 
+// redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
+// sagas
+import createSagaMiddleware from 'redux-saga';
+import {takeEvery, put} from 'redux-saga/effects';
 
+function* watcherSaga() {
 
+}
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+const storeInstance = createStore(
+    combineReducers({
+        
+    }),
+    // Add sagaMiddleware to our store
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('react-root'));
