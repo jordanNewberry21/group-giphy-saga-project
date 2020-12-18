ebb6f14d4d5e9e01d433511059cca69fd5865ae9
James Posey <jamesrposeyjr@gmail.com>
2020-12-18 11:25:12 -0600

    updated put

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -6,41 +6,49 @@ const router = express.Router();
 // return all favorite images
 router.get('/', (req, res) => {
   const sqlText = `SELECT * from favorite`;
-  pool.query(sqlText)
-  .then(result => {console.log(result.rows); res.send(result.rows)})
-  .catch(error => {console.log('error retrieving favorite giphys', error)})
+  pool
+    .query(sqlText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('error retrieving favorite giphys', error);
+    });
 });
 
-// add a new favorite 
+// add a new favorite
 router.post('/', (req, res) => {
   const gifToAdd = req.body.url;
-  console.log('adding new GIF to DB......', gifToAdd);
   let queryText = `INSERT INTO favorite (url)
                    VALUES ($1);`;
-  pool.query(queryText, [gifToAdd]).then(result => {
-    res.sendStatus(201);
-  }).catch(error => {
-    console.log('Error adding new feedback....', error);
-    res.sendStatus(500);
-  })
-  
+  pool
+    .query(queryText, [gifToAdd])
+    .then((result) => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log('Error adding new feedback....', error);
+      res.sendStatus(500);
+    });
 });
 
 // update given favorite with a category id
-router.put('/:favId', (req, res) => {
+router.put('/:id', (req, res) => {
   // req.body should contain a category_id to add to this favorite image
   const giphId = req.params.id;
   console.log(giphId);
   let sqlText = `UPDATE favorite 
                   SET category_id = $1
-                  WHERE id=$2`
-  pool.query(sqlText, [req.body.category_id, giphId])
-  .then(result => {res.sendStatus(200)})
-  .catch(error => { 
-    console.log('Error changing category, id', error);
-    res.sendStatus(500);
-  })
-  res.sendStatus(200);
+                  WHERE id=$2`;
+  pool
+    .query(sqlText, [req.body.category_id, giphId])
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log('Error changing category, id', error);
+      res.sendStatus(500);
+    });
 });
 
 // delete a favorite
